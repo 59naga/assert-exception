61d6c0c02c7235062ba47c4bbdd80a0fbfb990e5
59naga <i59naga@icloud.com>
2016-04-02 03:53:52 +0900

    feat(rejects): to implement the spec(BREAKING CHANGE)

--- src/index.js
+++ src/index.js
@@ -17,5 +17,13 @@ export function throws(exception) {
 * @returns {promise<reason>} reason - the rejected reason (rejects the fulfill)
 */
 export function rejects(promise) {
-  return new Promise((resolve, reject) => promise.then(reject, resolve));
+  if (typeof promise !== 'object' || typeof promise.then !== 'function') {
+    return Promise.reject(new TypeError('argument is not a promise'));
+  }
+
+  return promise
+  .then(
+    () => Promise.reject(new Error('Missing expected rejection..')),
+    (reason) => Promise.resolve(reason),
+  );
 }
